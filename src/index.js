dbd3a704e6c8de5267961804a3ef7beccf355c86
danPixl <d@danpixl.me>
2017-07-04 20:14:55 -0600

    minor tweaks, remove window frame

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Treble",
+    frame: false,
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
